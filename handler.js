046aec02a1033897bd06a0dd6e64f762c8e2a0df
hiroya.shimamoto <shshimamo@gmail.com>
2020-10-25 23:40:45 +0900

    Refactor handler.js

--- handler.js
+++ handler.js
@@ -8,11 +8,9 @@ module.exports.run = async ( event ) =>
 {
   const dataObject = JSON.parse( event.body );
 
-  // The response we will return to Slack
   let response = {
     statusCode: 200,
     body      : {},
-    // Tell slack we don't want retries, to avoid multiple triggers of this lambda
     headers   : { 'x-slack-no-retry': 1 }
   };
 
@@ -52,10 +50,6 @@ function verify( data )
   }
 }
 
-/**
- * Verifies the URL with a challenge - https://api.slack.com/events/url_verification
- * @param  {Object} data The event data
- */
 function verifyCall( data )
 {
   if (data.token === process.env.VERIFICATION_TOKEN) {
@@ -65,42 +59,42 @@ function verifyCall( data )
   }
 }
 
-/**
- * Process the message and executes an action based on the message received
- * @async
- * @param {Object} message The Slack message object
- */
 async function handleMessage(message) {
-  // Makes sure the bot was actually mentioned
-  if (!message.bot_id) {
-    // Gets the command from the message
-    let command = parseMessage(message.text);
+  if (message.bot_id) {
+    return;
+  }
 
-    // Executes differend commands based in the specified instruction
-    switch (command) {
-      case 'stop':
+  let command = parseMessage(message.text);
+
+  if (!['stop', 'start', 'status'].includes(command)) {
+    await sendSlackMessage(message.channel, 'stop, start, status を指定してください');
+    return;
+  }
+
+  const active = await isActive();
+  switch (command) {
+    case 'stop':
+      if (active) {
         await sendSlackMessage(message.channel, 'stop called');
-        break;
-      case 'start':
+      } else {
+        await sendSlackMessage(message.channel, '既に停止しています');
+      }
+      break;
+    case 'start':
+      if (active) {
+        await sendSlackMessage(message.channel, '既に稼働しています');
+      } else {
         await sendSlackMessage(message.channel, 'start called');
-        break;
-      case 'status':
-        const bool = await isActive();
-        await sendSlackMessage(message.channel, bool ? '稼働しています' : '停止しています');
-        break;
-      default:
-        await sendSlackMessage(message.channel, 'stop, start, status を指定してください');
-        break;
-    }
+      }
+      break;
+    case 'status':
+      await sendSlackMessage(message.channel, active ? '稼働しています' : '停止しています');
+      break;
+    default:
+      break;
   }
 }
 
-/**
- * Sends a message to Slack
- * @param  {String} channel
- * @param  {String} message
- * @return {Promise}
- */
 function sendSlackMessage(channel, message) {
   const params = {
     token: process.env.BOT_TOKEN,
@@ -112,36 +106,34 @@ function sendSlackMessage(channel, message) {
   return Slack.chat.postMessage(params);
 }
 
-/**
- * Parses the command/intent from the text of a message received by the bot
- * @param  {String} message
- * @return {String}
- */
 function parseMessage(message) {
   return message.split(' ', 2).pop();
 }
 
-async function isActive() {
+async function newPage() {
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
   });
+  return await browser.newPage();
+}
 
-  let page = await browser.newPage();
-
-  // pauseリンク
+async function login(page) {
   await page.goto('https://mergequeue.com/dashboard', {waitUntil: "domcontentloaded"});
   await page.type("input[name=username]", process.env.MERGE_QUEUE_EMAIL);
   await page.type("input[name=password]", process.env.MERGE_QUEUE_PASSWORD);
   page.click('button[type=submit]');
   await page.waitForNavigation({timeout: 60000, waitUntil: "domcontentloaded"});
+}
 
+async function isActive() {
+  let page = await newPage();
+  await login(page);
   let text = await page.$eval('.main .repo', item => {
     return item.textContent;
   });
-
   if(text.includes("YES")) {
     return true
   } else {
